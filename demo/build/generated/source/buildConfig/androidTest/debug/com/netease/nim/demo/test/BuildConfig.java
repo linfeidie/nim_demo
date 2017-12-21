e0ae20456baf0b10a7a05c7776406ba66c2f53fe
Bob Zheng <leavenotrace@foxmail.com>
2017-12-21 15:45:12 +0800

    Initial commit

---
+++ demo/build/generated/source/buildConfig/androidTest/debug/com/netease/nim/demo/test/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.netease.nim.demo.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.netease.nim.demo.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 40;
+  public static final String VERSION_NAME = "4.5.1";
+  // Fields from default config.
+  public static final String BUILD_DATE = "2017-12-21 8:56:46";
+  public static final String GIT_REVISION = "";
+}
