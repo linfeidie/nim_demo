1ab7c2052c5151403402d51ef663e26fce34bceb
Bob Zheng <leavenotrace@foxmail.com>
2017-12-22 16:36:55 +0800

    无报错 可以正常运行  展示登录

---
+++ uikit/src/com/netease/nim/uikit/common/activity/BastActivity.java
@@ -0,0 +1,11 @@
+package com.netease.nim.uikit.common.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bobzheng on 22/12/17.
+ */
+
+public class BastActivity extends AppCompatActivity {
+
+}
